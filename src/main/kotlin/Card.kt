34be76c45bf7e464b2c1c3633819e3821d9e9f1e
Jun Cheong <juncheong1@gmail.com>
2020-09-18 14:08:23 -0400

    Initial commit.

---
+++ src/main/kotlin/Card.kt
@@ -0,0 +1,4 @@
+class Card(var front: String?, var back: String?) {
+
+    constructor() : this(null, null)
+}
---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,41 @@
+import org.apache.commons.csv.CSVFormat
+import org.apache.commons.csv.CSVPrinter
+import java.io.File
+import java.io.FileWriter
+
+fun main() {
+    print("File path: ")
+    val filePath = readLine()!!
+    val outputPath = filePath.substring(0, filePath.indexOfLast { it == '\\' })
+    println("output path: $outputPath")
+
+    val cards = mutableListOf<Card>()
+    var card: Card? = null
+    File(filePath).forEachLine {line ->
+        if (line.isBlank() && card != null) {
+            cards.add(card!!)
+            card = null
+        } else {
+            if (card == null) {
+                card = Card()
+            }
+            if (card!!.front == null) {
+                card!!.front = line
+            } else {
+                card!!.back = line
+            }
+        }
+    }
+
+    card?.let { cards.add(it) }
+
+    toCsvFile(cards, outputPath)
+}
+
+fun toCsvFile(cards: List<Card>, outputPath: String) {
+    CSVPrinter(FileWriter("$outputPath\\output.csv"), CSVFormat.DEFAULT).use { printer ->
+        cards.forEach { card ->
+            printer.printRecord(card.front, card.back)
+        }
+    }
+}
