1e619c2441c0e8a5ddf99f292a87faaf1eaf46d7
Jun Cheong <juncheong1@gmail.com>
2020-11-07 08:36:13 -0500

    Adding module-info

---
+++ src/main/kotlin/module-info.java
@@ -0,0 +1,8 @@
+module ApplicationModule {
+    requires javafx.controls;
+    requires javafx.graphics;
+    requires tornadofx;
+    requires kotlin.stdlib;
+    requires commons.csv;
+    opens ui;
+}
